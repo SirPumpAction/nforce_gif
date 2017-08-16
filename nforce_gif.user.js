7a6eaf21a25ad1b92c4d2f3bc382477a1ad05a5d
SirPumpAction <banana4uall@gmail.com>
2017-08-16 02:43:08 +0200

    Create nforce_gif.user.js

---
+++ nforce_gif.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         NFOrce GIF
+// @namespace    http://www.nfohump.com/
+// @version      1.0.0
+// @description  Show webms inline
+// @author       https://github.com/SirPumpAction
+// @match        http://*.nfohump.com/forum/viewtopic.php*
+// @match        https://*.nfohump.com/forum/viewtopic.php*
+// @downloadURL  https://github.com/SirPumpAction/nforce_gif/raw/master/nforce_gif.user.js
+// @updateURL    https://github.com/SirPumpAction/nforce_gif/raw/master/nforce_gif.user.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    $('span.postbody a').each(function(i, link){
+        try {
+            var shorty = link.href.match(/detail\/(.*)/)[1];
+            var $video = $("<video>").attr({"loop":"", "muted":"", "playsinline":"", "preload":"", "poster":"https://thumbs.gfycat.com/" + shorty + "-small.gif", "width":"256", "style":"display: block;", "title":"CLICK: Play/Pause\nCTRL+CLICK: Enable/Disable controls"}).on('click', function(e){
+                var target = e.target;
+                if (!e.ctrlKey) {
+                    if (!target.playing) {
+                        target.playing = true;
+                        target.play();
+                        $(target).animate({
+                            "width": target.videoWidth
+                        }, 1000);
+                    } else {
+                        target.playing = false;
+                        $(target).animate({
+                            "width": "256px"
+                        }, 1000);
+                        setTimeout(function(){
+                            target.pause();
+                        },1000);
+                    }
+                } else {
+                    target.controls = !target.controls;
+                }
+            });
+            $video.append($("<source>").attr({"src": "https://fat.gfycat.com/" + shorty + ".webm", "type": "video/webm"}));
+            $video.append($('<source>').attr({"src":"https://thumbs.gfycat.com/" + shorty + "-mobile.mp4", "type":"video/mp4"}));
+            $video.append($('<source>').attr({"src":"https://giant.gfycat.com/" + shorty + ".mp4", "type":"video/mp4"}));
+            $video.append($('<img>').attr({"src":"https://thumbs.gfycat.com/" + shorty + "-small.gif", "type":"video/webm"}));
+            $(link).before($video,"Src: ");
+        } catch(e){}
+    });
+})();
