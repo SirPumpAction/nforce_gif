9f8d3c903fb886119928aa430c40f4cf4d9f9381
SirPumpAction <banana4uall@gmail.com>
2017-09-07 17:40:36 +0200

    Update nforce_gif.user.js
    now captures even more gfycat madness. whoa

--- nforce_gif.user.js
+++ nforce_gif.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOrce GIF
 // @namespace    http://www.nfohump.com/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Show webms inline
 // @author       https://github.com/SirPumpAction
 // @match        http://*.nfohump.com/forum/viewtopic.php*
@@ -21,6 +21,8 @@
             shorty = link.href.match(/gfycat\.com\/gifs\/detail\/(.*)/);
             if (!shorty)
                 shorty = link.href.match(/fat\.gfycat\.com\/(.*)\./);
+            if (!shorty)
+                shorty = link.href.match(/gfycat\.com\/(.*)$/);
             shorty = shorty[1];
             var $video = $("<video>").attr({"loop":"", "muted":"", "playsinline":"", "preload":"", "poster":"https://thumbs.gfycat.com/" + shorty + "-small.gif", "width":"256", "style":"display: block;", "title":"CLICK: Play/Pause\nCTRL+CLICK: Enable/Disable controls"}).on('click', function(e){
                 var target = e.target;
